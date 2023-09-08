b9c3ec772e7cb6e0c3bf400d1b3311b9d641a1e1
Charlie Greenman <charlie@razroo.com>
2023-09-08 16:01:17 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/test-joe-frazier-workspace-gen/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-joe-frazier-workspace-gen-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-joe-frazier-workspace-gen';
+}
